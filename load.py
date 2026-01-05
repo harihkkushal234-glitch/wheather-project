8c69e29c3e00d68f287a29c36bd344cbf5a8008f
Kushal H K <harihkkushal234@gmail.com>
2026-01-05 08:47:56 +0530

    Implement data loading function for SQLite
    Add function to load DataFrame into SQLite database

--- load.py
+++ load.py
@@ -1,1 +1,6 @@
+import sqlite3
 
+def load_data(df, db_name="weather_data.db"):
+    conn = sqlite3.connect(db_name)
+    df.to_sql("weather", conn, if_exists="append", index=False) #True for index Defult False
+    conn.close()
