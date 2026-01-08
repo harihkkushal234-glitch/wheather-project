037260e1d5055c69f7d8aabf3f0ddb32ce4b8198
Kushal H K <harihkkushal234@gmail.com>
2026-01-08 20:43:39 +0530

    Refactor load_data to handle city and state data

--- load.py
+++ load.py
@@ -1,6 +1,12 @@
 import sqlite3
 
-def load_data(df, db_name="weather_data.db"):
+def load_state_data(city_df, state_df, db_name="weather_data.db"):
     conn = sqlite3.connect(db_name)
-    df.to_sql("weather", conn, if_exists="append", index=False) #True for index Defult False
+
+    # Save raw city-level data
+    city_df.to_sql("weather_city", conn, if_exists="append", index=False)
+
+    # Save aggregated state-level data
+    state_df.to_sql("weather_state", conn, if_exists="append", index=False)
+
     conn.close()
